d99c201a2bb0e1729ca2c5d0559df4219c7f5ac3
Alexander <lsndrme@ya.ru>
2026-01-09 14:00:32 +0700

    test: add auto-generated e2e security tests
    skip-checks:true

---
+++ .brightsec/tests/post-xvwa-login-php.test.ts
@@ -0,0 +1,45 @@
+import { test, before, after } from 'node:test';
+import { SecRunner } from '@sectester/runner';
+import { AttackParamLocation, HttpMethod } from '@sectester/scan';
+
+const timeout = 40 * 60 * 1000;
+const baseUrl = process.env.BRIGHT_TARGET_URL!;
+
+let runner!: SecRunner;
+
+before(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!,
+    projectId: process.env.BRIGHT_PROJECT_ID!
+  });
+
+  await runner.init();
+});
+
+after(() => runner.clear());
+
+test('POST /xvwa/login.php', { signal: AbortSignal.timeout(timeout) }, async () => {
+  await runner
+    .createScan({
+      tests: ['csrf', 'sqli', 'xss', 'full_path_disclosure', 'secret_tokens'],
+      attackParamLocations: [AttackParamLocation.BODY],
+      starMetadata: {
+        code_source: 'lsndr/xvwa:master',
+        user_roles: {
+          roles: []
+        }
+      },
+      poolSize: +process.env.SECTESTER_SCAN_POOL_SIZE || undefined
+    })
+    .setFailFast(false)
+    .timeout(timeout)
+    .run({
+      method: HttpMethod.POST,
+      url: `${baseUrl}/xvwa/login.php`,
+      body: {
+        username: 'exampleUser',
+        password: 'examplePass'
+      },
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+    });
+});
