e45ea89ccc1208454cb43593e699e0368d029510
Alexander <lsndrme@ya.ru>
2026-01-09 13:52:47 +0700

    test: add auto-generated e2e security tests
    skip-checks:true

---
+++ .brightsec/tests/get-vulnerabilities-redirect-redirect-php.test.ts
@@ -0,0 +1,39 @@
+import { test, before, after } from 'node:test';
+import { SecRunner } from '@sectester/runner';
+import { AttackParamLocation, HttpMethod } from '@sectester/scan';
+
+const timeout = 40 * 60 * 1000;
+const baseUrl = process.env.BRIGHT_TARGET_URL!;
+
+let runner!: SecRunner;
+
+before(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!,
+    projectId: process.env.BRIGHT_PROJECT_ID!
+  });
+
+  await runner.init();
+});
+
+after(() => runner.clear());
+
+test('GET /vulnerabilities/redirect/redirect.php?forward=:url', { signal: AbortSignal.timeout(timeout) }, async () => {
+  await runner
+    .createScan({
+      tests: ['unvalidated_redirect', 'xss'],
+      attackParamLocations: [AttackParamLocation.QUERY],
+      starMetadata: {
+        code_source: 'lsndr/xvwa:master',
+        databases: ['MySQL'],
+        user_roles: { roles: [] }
+      },
+      poolSize: +process.env.SECTESTER_SCAN_POOL_SIZE || undefined
+    })
+    .setFailFast(false)
+    .timeout(timeout)
+    .run({
+      method: HttpMethod.GET,
+      url: `${baseUrl}/vulnerabilities/redirect/redirect.php?forward=http://example.com`
+    });
+});
